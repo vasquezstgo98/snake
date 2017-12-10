8fdd814fe484d4e7aab99858085877668ebb6893
Erik R <reyes.erik309@gmail.com>
2017-12-10 02:04:42 -0600

    Creacion de la clase ScoreView que mostrara las mejores puntuaciones

---
+++ src/com/isa/snake/view/ScoreView.java
@@ -0,0 +1,40 @@
+package com.isa.snake.view;
+
+/**
+* Clase AboutView
+* @author [Jorge, Marco, Erik, Carlos]
+* @version [0.1]
+*/
+import javax.swing.*;
+import java.awt.*;
+
+public class ScoreView extends JFrame{
+    public ScoreView(){
+        super("ScoreView");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(new BorderLayout());
+
+        JPanel results = new JPanel();
+        results.setLayout(new GridLayout(10 ,2, 5, 5));
+        results.add(new JLabel("NOMBRE", SwingConstants.CENTER));
+        results.add(new JLabel("PUNTUACION", SwingConstants.CENTER));
+        for(byte x = 2; x < 20; x++){
+            if(x%2 == 0 ){
+                results.add(new JLabel("NOMBRE DEL JUGADOR", SwingConstants.CENTER));
+            }else{
+                results.add(new JLabel("-------", SwingConstants.CENTER));
+            }
+        }
+        
+        JPanel regresar = new JPanel();
+        regresar.setLayout(new GridLayout());
+        regresar.add(new JButton("INICIO"));
+
+        Container opc = getContentPane();
+        opc.add(results, BorderLayout.CENTER);
+        opc.add(regresar, BorderLayout.SOUTH);
+    }
+}
